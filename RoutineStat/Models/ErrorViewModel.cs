8d5a98badb67126a8a66c4b69a3ef0046c43661d
Bogdan Kostadinov <bk7907k@gre.ac.uk>
2021-05-29 18:44:22 +0100

    Add project files.

---
+++ RoutineStat/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RoutineStat.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
