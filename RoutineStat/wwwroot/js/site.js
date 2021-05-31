943c1c4031424da8251a32a9373294b6269dfedb
Bogdan Kostadinov <bk7907k@gre.ac.uk>
2021-05-31 18:23:53 +0100

    add date/time picker

--- RoutineStat/wwwroot/js/site.js
+++ RoutineStat/wwwroot/js/site.js
@@ -1,4 +1,4 @@
-﻿var fp = flatpickr(document.querySelector('#flatpickr'), {
+﻿var bedTime = flatpickr(document.querySelector('.bedTime'), {
     //altFormat: "F j, Y",
     altInput: true,
     //inline: true,
@@ -10,7 +10,22 @@
     }
 });
 
+var wakeTime = flatpickr(document.querySelector('.wakeTime'), {
+    //altFormat: "F j, Y",
+    altInput: true,
+    //inline: true,
+    dateFormat: "F, d Y H:i",
+    enableTime: true,
+    //mode: "multiple",
+    onChange: function (selectedDates, dateStr, instance) {
+        console.log('date: ', dateStr);
+    }
+});
 
-$(".clear_button").click(function () {
-    fp.clear();
+$(".clear_button_bed").click(function () {
+    bedTime.clear();
+})
+
+$(".clear_button_wake").click(function () {
+    wakeTime.clear();
 })
