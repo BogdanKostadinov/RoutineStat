84b09990da9e27bf2eb2a027f669e0c828e950a2
Bogdan Kostadinov <bk7907k@gre.ac.uk>
2021-06-02 15:00:42 +0100

    added json package, fixed form layout

--- RoutineStat/wwwroot/js/site.js
+++ RoutineStat/wwwroot/js/site.js
@@ -1,31 +1,48 @@
-﻿var bedTime = flatpickr(document.querySelector('.bedTime'), {
-    //altFormat: "F j, Y",
+﻿//var bedTime = flatpickr(document.querySelector('.bedTime'), {
+//    //altFormat: "F j, Y",
+//    altInput: true,
+//    //inline: true,
+//    dateFormat: "F, d Y H:i",
+//    enableTime: true,
+//    //mode: "multiple",
+//    onChange: function (selectedDates, dateStr, instance) {
+//        console.log('date: ', dateStr);
+//    }
+//});
+
+var rangeTest = flatpickr(document.querySelector('.bedTime'), {
+    mode: "range",
+    minDate: "today",
+    dateFormat: "Y-m-d",
+    disable: [
+        function (date) {
+            // disable every multiple of 8
+            return !(date.getDate() % 8);
+        }
+    ],
     altInput: true,
-    //inline: true,
-    dateFormat: "F, d Y H:i",
     enableTime: true,
-    //mode: "multiple",
     onChange: function (selectedDates, dateStr, instance) {
         console.log('date: ', dateStr);
     }
 });
 
-var wakeTime = flatpickr(document.querySelector('.wakeTime'), {
-    //altFormat: "F j, Y",
-    altInput: true,
-    //inline: true,
-    dateFormat: "F, d Y H:i",
-    enableTime: true,
-    //mode: "multiple",
-    onChange: function (selectedDates, dateStr, instance) {
-        console.log('date: ', dateStr);
-    }
-});
+//var wakeTime = flatpickr(document.querySelector('.wakeTime'), {
+//    //altFormat: "F j, Y",
+//    altInput: true,
+//    //inline: true,
+//    dateFormat: "F, d Y H:i",
+//    enableTime: true,
+//    //mode: "multiple",
+//    onChange: function (selectedDates, dateStr, instance) {
+//        console.log('date: ', dateStr);
+//    }
+//});
 
 $(".clear_button_bed").click(function () {
     bedTime.clear();
 })
 
-$(".clear_button_wake").click(function () {
-    wakeTime.clear();
-})
+//$(".clear_button_wake").click(function () {
+//    wakeTime.clear();
+//})
